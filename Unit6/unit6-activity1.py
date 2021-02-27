ce8e887f0398bc292cf7b97a664302c80c9683f7
Shoroog Alghamdi <shoroogalghamdi@gmail.com>
2021-02-27 21:27:44 +0300

    Add files

---
+++ Unit6/unit6-activity1.py
@@ -0,0 +1,11 @@
+# Slide 55
+import turtle
+s = turtle.getscreen()
+t = turtle.Turtle()
+t.pencolor("purple")
+t.fillcolor("orange")
+t.pensize(10)
+t.speed(9)
+t.begin_fill()
+t.circle(90)
+t.end_fill()
---
+++ Unit6/unit6-activity2.py
@@ -0,0 +1,10 @@
+# Slide 58
+import turtle
+s = turtle.getscreen()
+t = turtle.Turtle()
+
+c = t.clone()
+t.color("magenta")
+c.color("red")
+t.circle(100)
+c.circle(60)
